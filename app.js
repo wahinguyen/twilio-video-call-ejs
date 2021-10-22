429a20d9fd262bd69d996ebcc065bc5355ee1876
Wahi <wahinguyen1999@gmail.com>
2021-10-22 11:48:30 +0700

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || "5000";
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
 app.use(express.static("./public"));
 
