824198c893388b34a09bb61d2bdae39693f6b5de
Wahi <wahinguyen1999@gmail.com>
2021-10-25 15:38:21 +0700

    deploy

--- public/assets/video.js
+++ public/assets/video.js
@@ -23,11 +23,11 @@ $(document).ready(function () {
   var localVideoTracks;
   Video.createLocalTracks().then((localTracks) => {
     localVideoTracks = localTracks;
-    // var localVideoTrack = localTracks.find((track) => track.kind === "video");
-    // localVideo.appendChild(localVideoTrack.attach());
-    localTracks.forEach((track) => {
-      localVideo.appendChild(track.attach());
-    });
+    var localVideoTrack = localTracks.find((track) => track.kind === "video");
+    localVideo.appendChild(localVideoTrack.attach());
+    // localTracks.forEach((track) => {
+    //   localVideo.appendChild(track.attach());
+    // });
     // localVideo.style = "display: none";
     // localVideo1.hide();
     // localAvatar.show();
