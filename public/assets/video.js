ebd761be72e7eeca06f9cd534b9a065de88f7b7a
Wahi <wahinguyen1999@gmail.com>
2021-10-25 15:43:56 +0700

    deploy

--- public/assets/video.js
+++ public/assets/video.js
@@ -21,9 +21,9 @@ $(document).ready(function () {
   var remoteVideo = document.getElementById("remote-video");
 
   var localVideoTracks;
-  Video.createLocalTracks().then((localTracks) => {
+  Video.createLocalVideoTrack().then((localTracks) => {
     localVideoTracks = localTracks;
-    var localVideoTrack = localTracks.find((track) => track.kind === "video");
+    //var localVideoTrack = localTracks.find((track) => track.kind === "video");
     localVideo.appendChild(localVideoTrack.attach());
     // localTracks.forEach((track) => {
     //   localVideo.appendChild(track.attach());
