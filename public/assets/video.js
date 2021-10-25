82186513c4a4485d70f6b3546a6a6b9d326bc47b
Wahi <wahinguyen1999@gmail.com>
2021-10-26 02:35:22 +0700

    deploy

--- public/assets/video.js
+++ public/assets/video.js
@@ -53,7 +53,14 @@ $(document).ready(function () {
 
   Twilio.Video.connect(token, connectOptions).then(
     (room) => {
-      playAudio();
+      const soundEffect = new Audio();
+      soundEffect.autoplay = true;
+      // onClick of first interaction on page before I need the sounds
+      // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
+      soundEffect.src =
+        "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+      // later on when you actually want to play a sound at any point without user interaction
+      soundEffect.src = "assets/HotlineBlingRingtone-DJ.mp3";
       console.log(`Room connected:`, room);
       //#region handle microphone
       btnMute.click(function () {
