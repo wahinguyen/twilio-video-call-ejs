3a3e02d99a85b69b9735585fd6718b76e8ba1232
Wahi <wahinguyen1999@gmail.com>
2021-10-25 16:45:34 +0700

    deploy

--- public/assets/video.js
+++ public/assets/video.js
@@ -176,16 +176,16 @@ $(document).ready(function () {
         });
         room.disconnect();
       });
-      Video.createLocalTracks().then((localTracks) => {
-        var localVideoTrack = localTracks.find(
-          (track) => track.kind === "video"
-        );
-        const container = document.getElementById("local-video");
-        container.appendChild(localVideoTrack.attach());
-        // localVideo.style = "display: none";
-        // localVideo1.hide();
-        // localAvatar.show();
-      });
+      // Video.createLocalTracks().then((localTracks) => {
+      //   var localVideoTrack = localTracks.find(
+      //     (track) => track.kind === "video"
+      //   );
+      //   const container = document.getElementById("local-video");
+      //   container.appendChild(localVideoTrack.attach());
+      //   // localVideo.style = "display: none";
+      //   // localVideo1.hide();
+      //   // localAvatar.show();
+      // });
     },
     (error) => {
       console.error(`Unable to connect to Room: ${error.message}`);
