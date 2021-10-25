27b0c5bd28f0d54a8fea6fb0d9613716ce6b2fbb
Wahi <wahinguyen1999@gmail.com>
2021-10-25 16:29:20 +0700

    deploy

--- public/assets/video.js
+++ public/assets/video.js
@@ -165,24 +165,22 @@ $(document).ready(function () {
         });
         room.disconnect();
       });
-
-      Video.createLocalTracks().then((localTracks) => {
-        // localVideoTracks = localTracks;
-        var localVideoTrack = localTracks.find(
-          (track) => track.kind === "video"
-        );
-        const container = document.getElementById("local-video");
-        container.appendChild(localVideoTrack.attach());
-        // localTracks.forEach((track) => {
-        //   localVideo.appendChild(track.attach());
-        // });
-        // localVideo.style = "display: none";
-        // localVideo1.hide();
-        // localAvatar.show();
-      });
     },
     (error) => {
       console.error(`Unable to connect to Room: ${error.message}`);
     }
   );
+
+  Video.createLocalTracks().then((localTracks) => {
+    // localVideoTracks = localTracks;
+    var localVideoTrack = localTracks.find((track) => track.kind === "video");
+    const container = document.getElementById("local-video");
+    container.appendChild(localVideoTrack.attach());
+    // localTracks.forEach((track) => {
+    //   localVideo.appendChild(track.attach());
+    // });
+    // localVideo.style = "display: none";
+    // localVideo1.hide();
+    // localAvatar.show();
+  });
 });
