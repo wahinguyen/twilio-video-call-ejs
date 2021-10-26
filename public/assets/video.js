186e85cbaa14d4113eab77b44ced21dfe651cfda
Wahi <wahinguyen1999@gmail.com>
2021-10-26 10:20:52 +0700

    deploy

--- public/assets/video.js
+++ public/assets/video.js
@@ -40,16 +40,16 @@ $(document).ready(function () {
     preferredVideoCodecs: ["VP8"],
     preferredAudioCodecs: ["OPUS"],
     name: "video call",
-    video: { name: "camera" },
-    audio: { name: "microphone" },
+    // video: { name: "camera" },
+    // audio: { name: "microphone" },
     // networkQuality: {
     //   local: 1, // LocalParticipant's Network Quality verbosity [1 - 3]
     //   remote: 2, // RemoteParticipants' Network Quality verbosity [0 - 3]
     // },
   };
-  if (!Twilio.Video.isSupported) {
-    alert("this browser not supported");
-  }
+  // if (!Twilio.Video.isSupported) {
+  //   alert("this browser not supported");
+  // }
   Twilio.Video.connect(token, connectOptions).then(
     (room) => {
       console.log(`Room connected:`, room);
